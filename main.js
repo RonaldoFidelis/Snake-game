e26dfcdbb89fd92be506b651cea456de447545d7
Ronaldo <ronaldofidelis.ti@gmail.com>
2023-05-30 17:45:11 -0300

    feat:ajusteScriptAddMenu

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ let player_score = document.querySelector('.score');
 let display_gameOver = document.querySelector('.game-over');
 let score_gameOver = document.querySelector('.score-gameOver');
 let btn_restart = document.querySelector('.restart-game');
+let form = document.querySelector('.start-game');
+let display_game = document.querySelector('.container');
+let btn_start = document.querySelector('.btn-start');
 
 // x = linha y = coluna
 let apple_x, apple_y;
@@ -10,7 +13,7 @@ let snake_x = 5, snake_y = 5;
 let snake_body = [0];
 let move_x = 0, move_y = 0;
 let score_init = 0;
-let speed_base = 125;
+let speed_base = 0;
 let game_over = false;
 
 function randomPositionApple() {
@@ -55,6 +58,24 @@ function checkEatApple(sx, sy, ax, ay) {
     return false;
 }
 
+function checkIfSnakeOutDisplay(sx, sy) {
+
+    if (sx <= 0 || sy > 30 || sy <= 0 || sx > 30) {
+        return true;
+    }
+
+    return false;
+};
+
+function checkHitBodySnake(x, sb) {
+
+    if (x != 0 && sb[0][1] === sb[x][1] && sb[0][0] === sb[x][0]) {
+        return true;
+    }
+
+    return false;
+};
+
 function endGame() {
     display_gameOver.style.display = 'flex';
     score_gameOver.textContent = `Você pegou ${score_init} maçãs.`;
@@ -72,7 +93,6 @@ function main() {
 
     let initPosition = `<div class="apple" style="grid-area: ${apple_y} / ${apple_x}"></div>`;
 
-    //Verificando se a cobra comeu a maçã
     if (checkEatApple(snake_x, snake_y, apple_x, apple_y)) {
         snake_body.push([apple_x, apple_y]);
     }
@@ -88,16 +108,15 @@ function main() {
     snake_x += move_x;
     snake_y += move_y;
 
-    //condição para para game over
-    if (snake_x <= 0 || snake_y > 30 || snake_y <= 0 || snake_x > 30) {
+    if (checkIfSnakeOutDisplay(snake_x, snake_y)) {
         game_over = true;
-    }
+    };
 
     for (let i = 0; i < snake_body.length; i++) {
         initPosition += `<div class="snake" style="grid-area: ${snake_body[i][1]} / ${snake_body[i][0]}"></div>`;
-        if (i != 0 && snake_body[0][1] === snake_body[i][1] && snake_body[0][0] === snake_body[i][0]) {
+        if (checkHitBodySnake(i, snake_body)) {
             game_over = true;
-        }
+        };
     }
 
     //Atrinbuindo a posição inicial no tabuleiro
@@ -105,5 +124,27 @@ function main() {
 }
 
 randomPositionApple();
-setInterval(main, speed_base);
+
+btn_start.addEventListener('click', () => {
+    console.log(document.querySelector('input[name="mod-game"]:checked').value);
+
+    if (document.querySelector('input[name="mod-game"]:checked').value == 'Dificil'){
+        form.style.display = 'none';
+        display_game.style.display = 'flex';
+        setInterval(main,100);
+    } else if (document.querySelector('input[name="mod-game"]:checked').value == 'Normal'){
+        form.style.display = 'none';
+        display_game.style.display = 'flex';
+        setInterval(main,135);
+    } else if (document.querySelector('input[name="mod-game"]:checked').value == 'Fácil'){
+        form.style.display = 'none';
+        display_game.style.display = 'flex';
+        setInterval(main,165);
+    }else if (document.querySelector('input[name="mod-game"]:checked')){
+        form.style.display = 'none';
+        display_game.style.display = 'flex';
+        setInterval(main,135);
+    }
+})
+
 document.addEventListener('keydown', keyPosition);
